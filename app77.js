df8450a304be6e0d8db77c3c09f80d4666421ee3
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-05-08 13:29:36 +0530

    Add files via upload

---
+++ app77.js
@@ -0,0 +1,18 @@
+let str = "apnacollege";
+
+function countVowels (str) {
+    let count = 0;
+    for (let i = 0; i<str.length; i++) {
+        if (
+            str.charAt(i) == "a" ||
+            str.charAt(i) == "e" ||
+            str.charAt(i) == "i" ||
+            str.charAt(i) == "o" ||
+            str.charAt(i) == "u"
+        )
+        {
+            count++;
+        }
+    }
+    return count;
+}
